cf0596d3ed4fc96a244ab21ff72c1c37588290a4
Peter Donis <peterdonis@alum.mit.edu>
2012-09-16 22:27:34 -0400

    Added autodiscovery code to setup.py; planning to factor out into separate setuputils module eventually.

--- setup.py
+++ setup.py
@@ -7,13 +7,23 @@ Released under the GNU General Public License, Version 2
 See the LICENSE and README files for more information
 """
 
+import os
 from datetime import datetime
 from itertools import islice, takewhile
 
-from distutils.core import setup
 
+name = "simpleblog"
 
 version = "0.2"
+
+description = "A simple Python blogging system."
+
+author = "Peter A. Donis"
+
+author_email = "peterdonis@alum.mit.edu"
+
+mdnames = ("CHANGES", "README")
+
 
 h3_prefix = '### '
 
@@ -44,7 +54,7 @@ def rst_from_md(basename):
         f.writelines(outlines)
 
 
-for basename in ("CHANGES", "README"):
+for basename in mdnames:
     rst_from_md(basename)
 
 
@@ -57,36 +67,88 @@ def long_description(filename="README", startline=5, endspec="Copyright and Lice
     ))
 
 
-setup(
-    name="simpleblog",
-    version=version,
-    description="A simple Python blogging system.",
-    long_description=long_description(),
-    author="Peter A. Donis",
-    author_email="peterdonis@alum.mit.edu",
-    url="http://pypi.python.org/pypi/simpleblog",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: POSIX",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python"
-    ],
-    requires=[
-        'plib (>=0.8.1)'
-    ],
-    packages=[
-        'simpleblog',
-        'simpleblog.commands',
-        'simpleblog.extensions'
-    ],
-    package_data={
-        'simpleblog': ["templates/*"]
-    },
-    scripts=[
-        'scripts/simpleblog-run'
+def pypi_url(name):
+    return "http://pypi.python.org/pypi/{}".format(name)
+
+
+classifiers="""
+Development Status :: 3 - Alpha
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: End Users/Desktop
+License :: OSI Approved :: GNU General Public License v2 (GPLv2)
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+Programming Language :: Python
+"""
+
+
+requires = """
+plib (>=0.8.1)
+"""
+
+listnames = ('classifiers', 'requires')
+
+
+for listname in listnames:
+    globals()[listname] = globals()[listname].strip().splitlines()
+
+
+def dir_to_package(dirname):
+    return dirname.replace(os.sep, '.')
+
+
+def package_paths(name):
+    return [
+        dirname
+        for dirname, _, filenames in os.walk(name)
+        if "__init__.py" in filenames
     ]
-)
+
+
+def autodiscover_packages(name):
+    return [
+        dir_to_package(dirname)
+        for dirname in package_paths(name)
+    ]
+
+
+def package_data_paths(pathname):
+    return [
+        "{}/*".format(dirname[len(pathname) + len(os.sep):])
+        for dirname, _, filenames in os.walk(pathname)
+        if "__init__.py" not in filenames
+    ]
+
+
+def autodiscover_package_data(name):
+    return dict(
+        (dir_to_package(pathname), package_data_paths(pathname))
+        for pathname in package_paths(name)
+        if package_data_paths(pathname)
+    )
+
+
+def autodiscover_scripts(dirname="scripts"):
+    return [
+        os.path.join(dirname, filename)
+        for filename in os.listdir(dirname)
+    ]
+
+
+if __name__ == '__main__':
+    from distutils.core import setup
+    setup(
+        name=name,
+        version=version,
+        description=description,
+        long_description=long_description(),
+        author=author,
+        author_email=author_email,
+        url=pypi_url(name),
+        classifiers=classifiers,
+        requires=requires,
+        packages=autodiscover_packages(name),
+        package_data=autodiscover_package_data(name),
+        scripts=autodiscover_scripts()
+    )
