0610711b5e813f1364e71b380398b24cca69f986
Peter Donis <peterdonis@alum.mit.edu>
2012-10-12 00:47:05 -0400

    Refactor serve-local command to ensure that current directory is always restored after server exits.

--- simpleblog/commands/serve_local.py
+++ simpleblog/commands/serve_local.py
@@ -9,10 +9,21 @@ See the LICENSE and README files for more information
 """
 
 import os
+from contextlib import contextmanager
 import SimpleHTTPServer
 import BaseHTTPServer
 
 from simpleblog.commands import BlogCommand
+
+
+@contextmanager
+def tmp_chdir(newdir):
+    oldcwd = os.getcwd()
+    os.chdir(newdir)
+    try:
+        yield
+    finally:
+        os.chdir(oldcwd)
 
 
 class ServeLocal(BlogCommand):
@@ -41,9 +52,7 @@ class ServeLocal(BlogCommand):
     )
     
     def run(self, blog):
-        oldcwd = os.getcwd()
         http_root = os.path.abspath(self.static_dir)
-        os.chdir(http_root)
         server_class = BaseHTTPServer.HTTPServer
         handler_class = SimpleHTTPServer.SimpleHTTPRequestHandler
         server_address = (self.opts.hostname, self.opts.port)
@@ -53,10 +62,9 @@ class ServeLocal(BlogCommand):
                 "http://{}:{:d}/".format(self.opts.hostname, self.opts.port)
             )
         httpd = server_class(server_address, handler_class)
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            if not self.opts.quiet:
-                print "Shutting down...."
-        finally:
-            os.chdir(oldcwd)
+        with tmp_chdir(http_root):
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                if not self.opts.quiet:
+                    print "Shutting down...."
