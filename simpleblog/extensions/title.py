4d5b996775707373abf6376e1952ea5c62e7a06a
Peter Donis <peterdonis@alum.mit.edu>
2012-10-30 22:26:42 -0400

    Added very simple em and strong formatting to title extension.

--- simpleblog/extensions/title.py
+++ simpleblog/extensions/title.py
@@ -8,6 +8,10 @@ Released under the GNU General Public License, Version 2
 See the LICENSE and README files for more information
 """
 
+import re
+
+from plib.stdlib.decotools import cached_property
+
 from simpleblog import BlogMixin, extendable_property, newline
 from simpleblog.caching import cached
 from simpleblog.extensions import BlogExtension
@@ -19,7 +23,8 @@ titles_file = BlogExtension.config.get('titles_file', u"titles")
 class TitleEntryMixin(BlogMixin):
     
     config_vars = dict(
-        title_separator=newline
+        title_separator=newline,
+        title_format=False
     )
     
     def _do_load(self):
@@ -30,12 +35,25 @@ class TitleEntryMixin(BlogMixin):
             self._titlestr = ""
         return raw
     
+    @cached_property
+    def title_rexps(self):
+        return map(re.compile, [
+            (r"\*([A-Za-z0-9]+)\*", "<em>\g<1></em>"),
+            (r"\*\*([A-Za-z0-9]+)\*\*", "<strong>\g<1></strong>")
+        ])
+    
+    def _format_title(self):
+        if self._titlestr and self.title_format:
+            for rexp, repl in self.title_rexps:
+                self._titlestr = rexp.sub(repl, self._titlestr)
+        return self._titlestr
+    
     @extendable_property(
         cached(titles_file)
     )
     def title(self):
         self.load()
-        return self._titlestr
+        return self._format_title()
 
 
 class TitleExtension(BlogExtension):
